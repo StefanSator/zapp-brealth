d9441a3bb4010644c7bfc45ca369b15e2a3188d7
Stefan Sator <stefansator@stefans-imac.speedport_w_724v_typ_a_05011603_05_028>
2018-05-07 19:30:27 +0200

    Rechnetask Funktionalität

--- app/src/main/java/com/example/stefansator/brealth/Addieren20Task.java
+++ app/src/main/java/com/example/stefansator/brealth/Addieren20Task.java
@@ -6,6 +6,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.view.animation.Animation;
 import android.view.animation.AnimationUtils;
+import android.widget.EditText;
 import android.widget.ImageSwitcher;
 import android.widget.ImageView;
 import android.widget.TextView;
@@ -18,12 +19,23 @@ import android.widget.ViewSwitcher;
 public class Addieren20Task extends AppCompatActivity {
     private ImageSwitcher switcher;
     private TextView aufgabe;
+    private EditText ergebnisfeld;
+    private static final int  LIMIT = 20;
+    private int aufgabeNr = 0;
+    private Additionsaufgabe rechenaufgaben[] = new Additionsaufgabe[10];
 
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_rechnen);
 
+        /* Initialize Array with Addition Tasks */
+        for (int i = 0 ; i < 10 ; i++) {
+            rechenaufgaben[i] = new Additionsaufgabe(LIMIT);
+        }
+
         aufgabe = findViewById(R.id.rechenaufgabe);
+        aufgabe.setText(rechenaufgaben[aufgabeNr].toString());
+
         switcher = findViewById(R.id.ImageSwitcher1);
         switcher.setFactory(new ViewSwitcher.ViewFactory() {
             @Override
@@ -45,8 +57,24 @@ public class Addieren20Task extends AppCompatActivity {
         switcher.setOutAnimation(out);
     }
 
-    public void showIfRight(View view) throws InterruptedException {
+    /* public void showIfRight(View view) throws InterruptedException {
         switcher.setImageResource(R.drawable.check);
         aufgabe.setText("1 + 1 =");
+    } */
+
+    public void showIfRight(View view) {
+        if (aufgabeNr == 10) return; /* Prüfe ob 10 Aufgaben bereits vorbeit */
+
+        ergebnisfeld = findViewById(R.id.rechenaufgabe_answer);
+        Integer ergebnis = Integer.parseInt(ergebnisfeld.getText().toString());
+
+        /* Control if result is right */
+        if (rechenaufgaben[aufgabeNr].getErgebnis() == ergebnis) {
+            switcher.setImageResource(R.drawable.check);
+            aufgabeNr++;
+            aufgabe.setText(rechenaufgaben[aufgabeNr].toString());
+        } else {
+            /* do nothing */
+        }
     }
 }
