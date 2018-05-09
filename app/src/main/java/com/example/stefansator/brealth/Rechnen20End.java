22cca4aed8e3a49648572d592e2504905c4d35f3
StefanSator <stefan.sator@web.de>
2018-05-09 18:23:04 +0200

    improved Result Screen

--- app/src/main/java/com/example/stefansator/brealth/Rechnen20End.java
+++ app/src/main/java/com/example/stefansator/brealth/Rechnen20End.java
@@ -10,7 +10,7 @@ import android.widget.TextView;
  */
 
 public class Rechnen20End extends AppCompatActivity {
-    private TextView anmerkungsFeld;
+    private TextView dauerFeld;
     private TextView falschFeld;
     private long bearbeitungsDauer;
     private int falseCounter;
@@ -24,10 +24,10 @@ public class Rechnen20End extends AppCompatActivity {
             falseCounter = getIntent().getExtras().getInt("falsch");
         }
 
-        anmerkungsFeld = findViewById(R.id.bemerkung_rechnenend);
-        anmerkungsFeld.setText("Du hast " + (bearbeitungsDauer / 1000) + " s gebraucht um die Übung zu beenden.");
-        falschFeld = findViewById(R.id.falsch_rechnenend);
-        falschFeld.setText("Du hast " + falseCounter + " Aufgaben falsch beantwortet.");
+        dauerFeld = findViewById(R.id.zeit_rechnen);
+        dauerFeld.setText("" + (bearbeitungsDauer/1000) + " s");
+        falschFeld = findViewById(R.id.falschCounter_rechnenend);
+        falschFeld.setText("" + falseCounter);
     }
 
     public void endResultScreen(View view) {
