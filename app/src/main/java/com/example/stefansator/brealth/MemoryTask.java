6a22543ca48c4df85b838f86e39fe6421b9e0d75
StefanSator <stefan.sator@web.de>
2018-05-15 19:25:48 +0200

    Revealing Memory Cards implemented

--- app/src/main/java/com/example/stefansator/brealth/MemoryTask.java
+++ app/src/main/java/com/example/stefansator/brealth/MemoryTask.java
@@ -12,17 +12,34 @@ import java.util.Random;
  */
 
 public class MemoryTask extends AppCompatActivity {
-    private String cards[];
+    private String cardPos[];
     private String cardNames[] = {"A", "A", "B", "B", "C", "C", "D", "D", "E", "E", "F", "F"};
     private int countCards;
+    private TextView memoryCard[];
 
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_memory);
 
         countCards = 12;
-        cards = new String[countCards];
+        cardPos = new String[countCards];
+        memoryCard = new TextView[countCards];
 
+        /* Initialize memoryCard Array */
+        memoryCard[0] = findViewById(R.id.memory_card1);
+        memoryCard[1] = findViewById(R.id.memory_card2);
+        memoryCard[2] = findViewById(R.id.memory_card3);
+        memoryCard[3] = findViewById(R.id.memory_card4);
+        memoryCard[4] = findViewById(R.id.memory_card5);
+        memoryCard[5] = findViewById(R.id.memory_card6);
+        memoryCard[6] = findViewById(R.id.memory_card7);
+        memoryCard[7] = findViewById(R.id.memory_card8);
+        memoryCard[8] = findViewById(R.id.memory_card9);
+        memoryCard[9] = findViewById(R.id.memory_card10);
+        memoryCard[10] = findViewById(R.id.memory_card11);
+        memoryCard[11] = findViewById(R.id.memory_card12);
+
+        /* Initialize Position of the memory cards */
         for (int i = 0 ; i < countCards ; i++) {
             int randomNumber = randomNumberGenerator(0, 11);
             while (cardNames[randomNumber].equals("empty")) {
@@ -31,7 +48,7 @@ public class MemoryTask extends AppCompatActivity {
             String name = cardNames[randomNumber];
             cardNames[randomNumber] = "empty";
 
-            cards[i] = name;
+            cardPos[i] = name;
         }
     }
 
@@ -41,30 +58,11 @@ public class MemoryTask extends AppCompatActivity {
     }
 
     public void revealMemoryCard(View view) {
-        TextView memoryText1 = findViewById(R.id.memory_card1);
-        TextView memoryText2 = findViewById(R.id.memory_card2);
-        TextView memoryText3 = findViewById(R.id.memory_card3);
-        TextView memoryText4 = findViewById(R.id.memory_card4);
-        TextView memoryText5 = findViewById(R.id.memory_card5);
-        TextView memoryText6 = findViewById(R.id.memory_card6);
-        TextView memoryText7 = findViewById(R.id.memory_card7);
-        TextView memoryText8 = findViewById(R.id.memory_card8);
-        TextView memoryText9 = findViewById(R.id.memory_card9);
-        TextView memoryText10 = findViewById(R.id.memory_card10);
-        TextView memoryText11 = findViewById(R.id.memory_card11);
-        TextView memoryText12 = findViewById(R.id.memory_card12);
-
-        memoryText1.setText(cards[0]);
-        memoryText2.setText(cards[1]);
-        memoryText3.setText(cards[2]);
-        memoryText4.setText(cards[3]);
-        memoryText5.setText(cards[4]);
-        memoryText6.setText(cards[5]);
-        memoryText7.setText(cards[6]);
-        memoryText8.setText(cards[7]);
-        memoryText9.setText(cards[8]);
-        memoryText10.setText(cards[9]);
-        memoryText11.setText(cards[10]);
-        memoryText12.setText(cards[11]);
+        for (int i = 0 ; i < countCards ; i++) {
+            if (memoryCard[i] == view) {
+                memoryCard[i].setText(cardPos[i]);
+            }
+        }
     }
+
 }
