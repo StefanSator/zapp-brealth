dace1564656ceac42aec17988aa13617c063d017
StefanSator <stefan.sator@web.de>
2018-05-16 20:12:33 +0200

    feature_memory

--- app/src/main/java/com/example/stefansator/brealth/MemoryTask.java
+++ app/src/main/java/com/example/stefansator/brealth/MemoryTask.java
@@ -4,8 +4,11 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import java.util.Random;
+import java.util.Timer;
+import java.util.TimerTask;
 
 /**
  * Created by StefanSator on 13.05.18.
@@ -16,6 +19,9 @@ public class MemoryTask extends AppCompatActivity {
     private String cardNames[] = {"A", "A", "B", "B", "C", "C", "D", "D", "E", "E", "F", "F"};
     private int countCards;
     private TextView memoryCard[];
+    /* variables which will later include index of drawn cards */
+    private int firstDraw = -1;
+    private int secondDraw = -1;
 
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -50,6 +56,14 @@ public class MemoryTask extends AppCompatActivity {
 
             cardPos[i] = name;
         }
+
+        new Timer().schedule(new TimerTask() {
+            @Override
+            public void run() {
+                // this code will be executed after 2 seconds
+
+            }
+        }, 2000);
     }
 
     private int randomNumberGenerator(int min, int max) {
@@ -57,12 +71,53 @@ public class MemoryTask extends AppCompatActivity {
         return random.nextInt(max - min + 1) + min;
     }
 
-    public void revealMemoryCard(View view) {
+    public void revealMemoryCard(View view) throws InterruptedException {
         for (int i = 0 ; i < countCards ; i++) {
             if (memoryCard[i] == view) {
                 memoryCard[i].setText(cardPos[i]);
+                if (firstDraw == -1) firstDraw = i;
+                else secondDraw = i;
             }
         }
+
+        /* 2 cards selected, now test if right cards where selected */
+        if (firstDraw != -1 && secondDraw != -1) {
+            checkMemoryCards();
+        }
+    }
+
+    private void checkMemoryCards() {
+        boolean test = testIfDrawnCardsAreRight(firstDraw, secondDraw);
+        if (test == true) {
+            disableRightCards(firstDraw, secondDraw);
+            Toast rightToast = Toast.makeText(getApplicationContext(), "Right", Toast.LENGTH_SHORT);
+            rightToast.show();
+        } else {
+            unrevealMemoryCards(firstDraw, secondDraw);
+            Toast falseToast = Toast.makeText(getApplicationContext(), "False", Toast.LENGTH_SHORT);
+            falseToast.show();
+        }
+        beginNewDraw();
+    }
+
+    private boolean testIfDrawnCardsAreRight(int first, int second) {
+        if (cardPos[first] == cardPos[second]) return true;
+        return false;
+    }
+
+    private void beginNewDraw() {
+        firstDraw = -1;
+        secondDraw = -1;
+    }
+
+    private void disableRightCards(int first, int second) {
+        memoryCard[first].setClickable(false);
+        memoryCard[second].setClickable(false);
+    }
+
+    private void unrevealMemoryCards(int first, int second) {
+        memoryCard[first].setText("/@");
+        memoryCard[second].setText("/@");
     }
 
 }
