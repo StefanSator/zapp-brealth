d51db982d7992ee767207c8574075e02bcf518f3
Raffael Diestelmann <dir43378@hs-regensburg.de>
2018-05-22 12:42:27 +0200

    Implemented Nährstoffzentrale with Fragments

---
+++ app/src/main/java/com/example/stefansator/brealth/NahrungsMittelFragment.java
@@ -0,0 +1,18 @@
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.stefansator.brealth.R;
+
+public class NahrungsMittelFragment extends Fragment {
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_nahrungsmittel, null);
+    }
+}
