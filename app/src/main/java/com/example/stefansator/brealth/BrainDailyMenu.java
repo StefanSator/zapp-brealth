3ce2a2625c306490925a4284bd336b88b93aff3c
StefanSator <stefan.sator@web.de>
2018-05-25 22:45:20 +0200

    Automatic daily notification implemented

--- app/src/main/java/com/example/stefansator/brealth/BrainDailyMenu.java
+++ app/src/main/java/com/example/stefansator/brealth/BrainDailyMenu.java
@@ -1,12 +1,12 @@
 package com.example.stefansator.brealth;
 
-import android.app.NotificationManager;
+import android.app.AlarmManager;
 import android.app.PendingIntent;
 import android.content.Intent;
 import android.os.Bundle;
-import android.support.v4.app.NotificationCompat;
 import android.support.v7.app.AppCompatActivity;
-import android.view.View;
+
+import java.util.Calendar;
 
 /**
  * Created by stefansator on 25.05.18.
@@ -17,23 +17,15 @@ public class BrainDailyMenu extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_braindailymenu);
-    }
 
-    public void sendNotification(View view) {
-        Notifi("Hallo dies ist eine Benachrichtigung");
-    }
-
-    public void Notifi(String Nachricht) {
-        final Intent notificationIntent = new Intent(BrainDailyMenu.this, BrainDailyMenu.class);
-        final PendingIntent pendingIntent = PendingIntent.getActivity(BrainDailyMenu.this, 0, notificationIntent, 0);
-        NotificationCompat.Builder mBuilder = new NotificationCompat.Builder(BrainDailyMenu.this);
-        mBuilder.setSmallIcon(R.drawable.ic_launcher_foreground);
-        mBuilder.setContentTitle("Brealth");
-        mBuilder.setContentIntent(pendingIntent);
-        mBuilder.setContentText(Nachricht);
-
-        NotificationManager mNotificationManager = (NotificationManager) BrainDailyMenu.this.getSystemService(BrainDailyMenu.this.NOTIFICATION_SERVICE);
-        mNotificationManager.notify(666, mBuilder.build());
+        Calendar calendar = Calendar.getInstance();
+        calendar.set(Calendar.HOUR_OF_DAY, 12);
+        calendar.set(Calendar.MINUTE, 15);
+        calendar.set(Calendar.SECOND, 0);
+        Intent notiIntent = new Intent(BrainDailyMenu.this, AlarmReceiver.class);
+        PendingIntent pendingIntent = PendingIntent.getBroadcast(BrainDailyMenu.this, 0, notiIntent, PendingIntent.FLAG_UPDATE_CURRENT);
+        AlarmManager alarmManager = (AlarmManager) BrainDailyMenu.this.getSystemService(BrainDailyMenu.this.ALARM_SERVICE);
+        alarmManager.setRepeating(AlarmManager.RTC_WAKEUP, calendar.getTimeInMillis(), AlarmManager.INTERVAL_DAY, pendingIntent);
 
     }
 }
