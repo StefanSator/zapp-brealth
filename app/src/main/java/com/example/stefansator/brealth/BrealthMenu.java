9e870d760b62c485261304f74e4994a82dc15f13
Julian Sellner <julian.sellner@st.oth-regensburg.de>
2018-05-31 00:21:51 +0900

    updated startscreen added brealth menu

--- app/src/main/java/com/example/stefansator/brealth/BrealthMenu.java
+++ app/src/main/java/com/example/stefansator/brealth/BrealthMenu.java
@@ -1,16 +1,24 @@
 package com.example.stefansator.brealth;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-
-/**
- * Created by stefansator on 04.05.18.
- */
+import android.view.View;
 
 public class BrealthMenu extends AppCompatActivity {
 
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_brealthmenu);
+    }
+
+    public void gotoTest(View view) {
+        Intent testIntent = new Intent( BrealthMenu.this, BrealthTest.class);
+        BrealthMenu.this.startActivity(testIntent);
+    }
+
+    public void gotoUebungen(View view) {
+        Intent uebungenIntent = new Intent( BrealthMenu.this, BrealthUebungen.class);
+        BrealthMenu.this.startActivity(uebungenIntent);
     }
 }
