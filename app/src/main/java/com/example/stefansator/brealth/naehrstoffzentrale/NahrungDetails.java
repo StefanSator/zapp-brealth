9ce57d656e63346a9d0ef4174b7435fe9ffaa037
DiestelmannRaffael <raffael@diestelmann.de>
2018-06-15 11:23:41 +0200

    Added NahrungDetails

---
+++ app/src/main/java/com/example/stefansator/brealth/naehrstoffzentrale/NahrungDetails.java
@@ -0,0 +1,16 @@
+package com.example.stefansator.brealth.naehrstoffzentrale;
+
+import android.app.ActionBar;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.example.stefansator.brealth.R;
+
+public class NahrungDetails extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_nahrung_details);
+    }
+}
--- app/src/main/java/com/example/stefansator/brealth/naehrstoffzentrale/NahrungsMittelFragment.java
+++ app/src/main/java/com/example/stefansator/brealth/naehrstoffzentrale/NahrungsMittelFragment.java
@@ -1,5 +1,6 @@
 package com.example.stefansator.brealth.naehrstoffzentrale;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
@@ -85,37 +86,23 @@ public class NahrungsMittelFragment extends Fragment {
                 .addConverterFactory(GsonConverterFactory.create())
                 .build();
 
-        RetrofitObjectAPI service = retrofit.create(RetrofitObjectAPI.class);
+        final RetrofitObjectAPI service = retrofit.create(RetrofitObjectAPI.class);
         Call<Model> call = service.getJsonObjectData(getFoodName(), "bfe5aa37", "c9cc3f70b3bf3964b7f583de92b22f10", 1);
 
         call.enqueue(new Callback<Model>() {
             @Override
             public void onResponse(Call<Model> call, Response<Model> response) {
                 hintList = response.body().getHints();
-                if(!hintList.isEmpty()) {
-                    setFatText(0);
-                    setProteinText(0);
-                    setCarbsText(0);
-                } else {
-                    Log.d("mainAct", "test - empty");
-                }
 
-                ListView listView = (ListView) getView().findViewById(R.id.foodList);
-                adapter = new ArrayAdapter<String>(getContext(), android.R.layout.simple_list_item_1, foodItems);
-                adapter.clear();
-                for(int i = 0; i < hintList.size(); i++) {
-                    adapter.add(hintList.get(i).getFood().getLabel());
-                }
-                listView.setAdapter(adapter);
-                listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-                    @Override
-                    public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-                        setFatText(position);
-                        setProteinText(position);
-                        setCarbsText(position);
-                        Log.d("blabla", adapter.getItem(position).toString());
-                    }
-                });
+//                if(!hintList.isEmpty()) {
+//                    setFatText(0);
+//                    setProteinText(0);
+//                    setCarbsText(0);
+//                } else {
+//                    Log.d("mainAct", "test - empty");
+//                }
+
+                setListView();
             }
 
             @Override
@@ -125,36 +112,65 @@ public class NahrungsMittelFragment extends Fragment {
         });
     }
 
-    private void setFatText(int position) {
-        TextView fatView = (TextView) getView().findViewById(R.id.textViewFat);
-        Double fat = hintList.get(position).getFood().getNutrients().getFat();
-
-        if (fat == null) {
-            fatView.setText("N.A.");
-        } else {
-            fatView.setText(String.format("%.2f",fat));
+    private void setListView() {
+        ListView listView = (ListView) getView().findViewById(R.id.foodList);
+        adapter = new ArrayAdapter<String>(getContext(), android.R.layout.simple_list_item_1, foodItems);
+        adapter.clear();
+        for(int i = 0; i < hintList.size(); i++) {
+            adapter.add(hintList.get(i).getFood().getLabel());
         }
+        listView.setAdapter(adapter);
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+//                Bundle foodBundle = getFoodInfo(position);
+
+                Intent zentraleIntent = new Intent(getActivity(), NahrungDetails.class);
+                startActivity(zentraleIntent);
+//                setFatText(position);
+//                setProteinText(position);
+//                setCarbsText(position);
+                Log.d("blabla", adapter.getItem(position).toString());
+            }
+        });
     }
 
-    private void setProteinText(int position) {
-        TextView proteinView = (TextView) getView().findViewById(R.id.textViewProtein);
-        Double protein = hintList.get(position).getFood().getNutrients().getProtein();
+//    private Bundle getFoodInfo(int position) {
+//        Bundle b = new Bundle();
+//        b.putInt("amount", hintList.get(position).getMeasures().);
+//    }
 
-        if (protein == null) {
-            proteinView.setText("N.A.");
-        } else {
-            proteinView.setText(String.format("%.2f",protein));
-        }
-    }
 
-    private void setCarbsText(int position) {
-        TextView carbsView = (TextView) getView().findViewById(R.id.textViewCarbs);
-        Double carbs = hintList.get(position).getFood().getNutrients().getCarbohydrates();
-
-        if (carbs == null) {
-            carbsView.setText("N.A.");
-        } else {
-            carbsView.setText(String.format("%.2f",carbs));
-        }
-    }
+//    private void setFatText(int position) {
+//        TextView fatView = (TextView) getView().findViewById(R.id.textViewFat);
+//        Double fat = hintList.get(position).getFood().getNutrients().getFat();
+//
+//        if (fat == null) {
+//            fatView.setText("N.A.");
+//        } else {
+//            fatView.setText(String.format("%.2f",fat));
+//        }
+//    }
+//
+//    private void setProteinText(int position) {
+//        TextView proteinView = (TextView) getView().findViewById(R.id.textViewProtein);
+//        Double protein = hintList.get(position).getFood().getNutrients().getProtein();
+//
+//        if (protein == null) {
+//            proteinView.setText("N.A.");
+//        } else {
+//            proteinView.setText(String.format("%.2f",protein));
+//        }
+//    }
+//
+//    private void setCarbsText(int position) {
+//        TextView carbsView = (TextView) getView().findViewById(R.id.textViewCarbs);
+//        Double carbs = hintList.get(position).getFood().getNutrients().getCarbohydrates();
+//
+//        if (carbs == null) {
+//            carbsView.setText("N.A.");
+//        } else {
+//            carbsView.setText(String.format("%.2f",carbs));
+//        }
+//    }
 }
